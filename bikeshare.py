9c4f3530ab7dc2250397daaf719852d69aab9a3d
root <root@localhost>
2024-06-25 15:34:59 +0000

    Updated commetns for Chicago

--- bikeshare.py
+++ bikeshare.py
@@ -8,6 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     #Asks user to specify a city, month, and day to analyze.
+    # Currently this will work only for Chicago
     """
     Returns:
         (str) city - name of the city to analyze
